07d2bf7f29bf2f2e4a6463ff7e8f9013199f7f21
Opeyemi Saliu <saliuoazeez@gmail.com>
2026-07-05 12:54:53 +0100

    Configured celery tasks to be published to billr queue.

--- boot.sh
+++ boot.sh
@@ -3,7 +3,7 @@
 
 /home/opeyemi/.pyenv/versions/billr/bin/uvicorn config.asgi:application --reload --host 0.0.0.0 --port 8000 &
 
-/home/opeyemi/.pyenv/versions/billr/bin/celery -A config worker --loglevel=info
+/home/opeyemi/.pyenv/versions/billr/bin/celery -A config worker -Q billr --loglevel=info
 
 
 
--- utilities/tasks.py
+++ utilities/tasks.py
@@ -12,6 +12,6 @@ load_dotenv()
 EMAIL_SENDER = os.getenv("EMAIL_HOST_USER")
 
 
-@shared_task
+@shared_task(queue="billr")
 def send_email(subject,body,recipients,from_email=EMAIL_SENDER):
     send_mail(subject=subject,message=body,from_email=from_email,recipient_list=recipients)
